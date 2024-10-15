adeac11b4babc2bc6085c616a6e7e9a2bc80f2ce
arnau2104 <amarques20733@iesjoanramis.org>
2024-10-15 19:38:27 +0200

    main acabat

--- UD1/Aula Museu Arnau/JS/menu.js
+++ UD1/Aula Museu Arnau/JS/menu.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menuIcon = document.querySelector(".mobile-menu");
     const displayedMenu = document.querySelector("#menu-click");
-    const menuClick = document.querySelector(".fa-times");
+    const menuClick = document.querySelector(".close-menu");
     const userMenu = document.querySelector(".fa-user");
     const userMenuClick = document.querySelector(".user-menu-click");
+    const userMenuClose = document.querySelector(".close-menu-user");
   
     //Mostrar menu
     menuIcon.addEventListener("click", function() {
@@ -17,6 +18,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
     //Mostrar menu usuari
     userMenu.addEventListener("click",function(){
+        userMenuClick.classList.toggle("active")
+    })
+
+    //Tancar menu usuari
+    userMenuClose.addEventListener("click",function(){
         userMenuClick.classList.toggle("active")
     })
   });
