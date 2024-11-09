44bf64fdd2f5ace37f8815db9b086594efeaa9cf
arnau2104 <amarques20733@iesjoanramis.org>
2024-11-09 12:53:13 +0100

    contrast colors in progres

--- UD4/Activity2/Aula_Museu_Arnau/JS/accesibility.js
+++ UD4/Activity2/Aula_Museu_Arnau/JS/accesibility.js
@@ -4,6 +4,12 @@ let accesibilityIcon = document.getElementById("accesibilityIcon");
 let accesibility = document.querySelector(".accesibility");
 let close = document.querySelector(".accesibility .fa-times");
 let darkContrast = document.querySelector(".dark-contrast");
+// let logo = document.querySelector(".logo");
+let lightContrast = document.querySelector(".light-contrast");
+let highSaturation = document.querySelector(".high-saturation");
+let header = document.querySelector("header");
+let main = document.querySelector("main");
+let footer = document.querySelector("footer");
 
 accesibilityIcon.addEventListener('click' ,()=>{
     accesibility.classList.toggle("active");
@@ -21,9 +27,53 @@ sliderRange.oninput = function(){
 
 darkContrast.addEventListener('click' , ()=>{
 
-    
-
     document.body.classList.toggle("dark-mode");
     accesibility.classList.toggle("active");
+    // logo.setAttribute ('src', './Assets/Imgs/logo_clar.png');
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("darkMode", "enabled");
+    } else {
+        localStorage.setItem("darkMode", "disabled");
+    };
   
 });
+ 
+
+
+lightContrast.addEventListener('click', ()=>{
+    document.body.classList.remove("dark-mode");
+    accesibility.classList.toggle("active");
+    localStorage.setItem("darkMode", "disabled");
+
+});
+
+//Posar clase high-saturation al body
+highSaturation.addEventListener('click', ()=>{
+    header.classList.toggle("high-saturation");
+    main.classList.toggle("high-saturation");
+    footer.classList.toggle("high-saturation");
+    accesibility.classList.toggle("active");
+    console.log("dins");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("highsaturation", "enabled");
+    } else {
+        localStorage.setItem("highSaturation", "disabled");
+    };
+    
+
+});
+
+
+
+// Al cargar la página, aplica el modo oscuro si estaba habilitado
+window.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem("darkMode") === "enabled") {
+        document.body.classList.add("dark-mode");
+    }else if (localStorage.getItem("highSaturation") === "enabled") {
+        document.body.classList.add("high-saturation");
+        };
+    });
+// Al cargar la página, aplica el modo oscuro si estaba habilitado
+
+
