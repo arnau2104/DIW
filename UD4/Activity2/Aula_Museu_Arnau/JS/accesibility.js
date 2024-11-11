f7826f2662f51d403a0fb9fcf6066d6587a77922
arnau2104 <amarques20733@iesjoanramis.org>
2024-11-11 21:03:43 +0100

    grey-scale fet

--- UD4/Activity2/Aula_Museu_Arnau/JS/accesibility.js
+++ UD4/Activity2/Aula_Museu_Arnau/JS/accesibility.js
@@ -1,69 +1,19 @@
+
 let sliderRange = document.getElementById("sliderRange");
 let rangeValue = document.getElementById("rangeValue");
-let accesibilityIcon = document.getElementById("accesibilityIcon");
+let accesibilityIcon = document.getElementById('accesibilityIcon');
 let accesibility = document.querySelector(".accesibility");
 let close = document.querySelector(".accesibility .fa-times");
 let darkContrast = document.querySelector(".dark-contrast");
 // let logo = document.querySelector(".logo");
 let lightContrast = document.querySelector(".light-contrast");
 let highSaturation = document.querySelector(".high-saturation");
+let lowSaturation = document.querySelector(".low-saturation");
 let header = document.querySelector("header");
 let main = document.querySelector("main");
 let footer = document.querySelector("footer");
-
-accesibilityIcon.addEventListener('click' ,()=>{
-    accesibility.classList.toggle("active");
-});
-
-close.addEventListener('click', ()=>{
-    accesibility.classList.toggle("active");
-});
-
-rangeValue.innerText = sliderRange.value;
-
-sliderRange.oninput = function(){
-    rangeValue.innerText = sliderRange.value;
-};
-
-darkContrast.addEventListener('click' , ()=>{
-
-    document.body.classList.toggle("dark-mode");
-    accesibility.classList.toggle("active");
-    // logo.setAttribute ('src', './Assets/Imgs/logo_clar.png');
-    if (document.body.classList.contains("dark-mode")) {
-        localStorage.setItem("darkMode", "enabled");
-    } else {
-        localStorage.setItem("darkMode", "disabled");
-    };
-  
-});
- 
-
-
-lightContrast.addEventListener('click', ()=>{
-    document.body.classList.remove("dark-mode");
-    accesibility.classList.toggle("active");
-    localStorage.setItem("darkMode", "disabled");
-
-});
-
-//Posar clase high-saturation al body
-highSaturation.addEventListener('click', ()=>{
-    header.classList.toggle("high-saturation");
-    main.classList.toggle("high-saturation-main");
-    footer.classList.toggle("high-saturation");
-    accesibility.classList.toggle("active");
-    console.log("dins");
-
-    if (document.body.classList.contains("dark-mode")) {
-        localStorage.setItem("highsaturation", "enabled");
-    } else {
-        localStorage.setItem("highSaturation", "disabled");
-    };
-    
-
-});
-
+let greyScale = document.querySelector(".grey-scale"); 
+console.log("js cargat");
 
 
 // Al cargar la página, aplica el modo oscuro si estaba habilitado
@@ -71,9 +21,87 @@ window.addEventListener('DOMContentLoaded', () => {
     if (localStorage.getItem("darkMode") === "enabled") {
         document.body.classList.add("dark-mode");
     }else if (localStorage.getItem("highSaturation") === "enabled") {
-        document.body.classList.add("high-saturation");
+        header.classList.add("high-saturation");
+        main.classList.add("high-saturation");
+        footer.classList.add("high-saturation");
         };
+    
+
+    accesibilityIcon.addEventListener('click' ,()=>{
+        console.log("click btn access");
+
+        accesibility.classList.toggle("active");
     });
-// Al cargar la página, aplica el modo oscuro si estaba habilitado
+    
+    close.addEventListener('click', ()=>{
+        accesibility.classList.toggle("active");
+    });
+    
+    rangeValue.innerText = sliderRange.value;
+    
+    sliderRange.oninput = function(){
+        rangeValue.innerText = sliderRange.value;
+    };
+    
+    //darkcontrast
+    darkContrast.addEventListener('click' , ()=>{
+    
+        document.body.classList.toggle("dark-mode");
+        accesibility.classList.toggle("active");
+        // logo.setAttribute ('src', './Assets/Imgs/logo_clar.png');
+        if (document.body.classList.contains("dark-mode")) {
+            localStorage.setItem("darkMode", "enabled");
+        } else {
+            localStorage.setItem("darkMode", "disabled");
+        };
+      
+    });
+     
+    
+    //lightcontrast
+    lightContrast.addEventListener('click', ()=>{
+        document.body.classList.remove("dark-mode");
+        accesibility.classList.toggle("active");
+        localStorage.setItem("darkMode", "disabled");
+    
+    });
+    
+    //Posar clase high-saturation 
+    highSaturation.addEventListener('click', ()=>{
+        header.classList.toggle("high-saturation");
+        main.classList.toggle("high-saturation");
+        footer.classList.toggle("high-saturation");
+       
+        accesibility.classList.toggle("active");
+        console.log("dins");
+    
+        if (header.classList.contains("high-saturation")) {
+            localStorage.setItem("highSaturation", "enabled");
+        } else {
+            localStorage.setItem("highSaturation", "disabled");
+        };
+        
+    
+    });
+
+    lowSaturation.addEventListener('click', ()=>{
+        header.classList.toggle("low-saturation");
+        main.classList.toggle("low-saturation");
+        footer.classList.toggle("low-saturation");
+        accesibility.classList.toggle("active");
+        console.log("low saturation");
+    });
+
+    greyScale.addEventListener('click', ()=>{
+        header.classList.toggle("grey-scale");
+        main.classList.toggle("grey-scale");
+        footer.classList.toggle("grey-scale");
+        accesibility.classList.toggle("active");
+        console.log("grey-scale");
+    });
+
+
+});
+
 
 
