05406991196bc6e7cb2b2af5b472017232df28ed
arnau2104 <amarques20733@iesjoanramis.org>
2024-11-18 20:55:13 +0100

    primeres pases amb tailwind

---
+++ UD3/Activity2/Aula_Museu_Arnau/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Pages/Noticies.html"],
+  theme: {
+    extend: {
+      colors: {
+        "light-white" : "#FAF9F9"
+      }
+    },
+  },
+  plugins: [],
+}
+
